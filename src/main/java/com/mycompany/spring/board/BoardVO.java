15ba31a1d6dc6d11642acd9b2342247aec143416
SH <22000786@handong.edu>
2021-12-10 17:15:56 +0900

    add BoardVO.java

---
+++ src/main/java/com/mycompany/spring/board/BoardVO.java
@@ -0,0 +1,62 @@
+package com.mycompany.spring.board;
+
+public class BoardVO {
+	private int id;
+	private String regdate;
+	private String name;
+	private String music;
+	private String gender;
+	private int age;
+	private String phoneNum;
+	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMusic() {
+		return music;
+	}
+	public void setMusic(String music) {
+		this.music = music;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
